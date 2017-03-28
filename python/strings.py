091dbee13073b3d04dda08e4d4bc057fbbb8fbdb
Kelly Garrett <kelly.garrett@utexas.edu>
2017-03-28 14:48:13 -0500

    Create strings.py

---
+++ python/strings.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+print("Hello Data Wrangling")
+
+print("Hello, Kelly, welcome to Python.")
+
+welcomeMsg = "Hello, Kelly, welcome to Python."
+# = assignment
+# taking something and putting it in a variable
+
+print(welcomeMsg)
+
+longerMsg = welcomeMsg + " " + "I hope you enjoy yourself."
+
+print(longerMsg)
+
+# Each print statement creates a new line. Hitting return in the script does not create a new line.
+# Return is a meaningful character to Python
+# Instead, us \n
+
+# Commenting - lines are ignored with the hash character (#)
+# Do not comment out variables to keep a print line
+
+# Error message - type of error at bottom, line, and message about error
+
+yourName = input("What's your name, then?")
+# spits out the prompt to the terminal, then waits for user to enter some text and hit return, before ending the program
+
+print("Hello, " + yourName + ", welcome to Python.")
